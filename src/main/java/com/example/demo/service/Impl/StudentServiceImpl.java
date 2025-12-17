10da68301427e5bc9393195c18b825c2447ca8e4
bala-amypo <bala@amypo.in>
2025-12-17 09:51:04 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -1,12 +1,23 @@
-package com.example.demo.service.Impl;
+package com.example.demo.service;
 
+import com.example.demo.entity.StudentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.sterotype.Service;
-import com.example.demo.service.StudentService;
+import com.example.demo.Service.StudentService;
 
 
-@Service
-public class StudentServiceImpl implements StudentService{
-    StudentEntity postdata(StudentEntity stu);
+public interface StudentService{
+    @Autowired StudentRepo student;
+    // save()
+    // findall()
+    // findById()
+    // deleteById()
+    // existsById()
 
+    @Override
+    public StudentEntity postdata(StudentEntity stu){
+        return student.save(stu);  
+
+    }
 }
 
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,21 +1,11 @@
-package com.example.demo.service;
+package com.example.demo.service.Impl;
 
-import com.example.demo.entity.StudentEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.sterotype.Service;
-import com.example.demo.Service.StudentService;
+import com.example.demo.service.StudentService;
 
 
-public interface StudentService{
-    @Autowired StudentRepo student;
-    // save()
-    // findall()
-    // findById()
-    // deleteById()
-    // existsById()
+@Service
+public class StudentServiceImpl implements StudentService{
+    StudentEntity postdata(StudentEntity stu);
 
-    public StudentEntity postdata(StudentEntity stu){
-        return student.save(stu);  
-
-    }
 }
